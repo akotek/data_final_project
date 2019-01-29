e7e7457e8a534a82278701dadaba5ab8b0aa44e6
kotek <aviv.kotek@cs.huji.ac.il>
2019-01-29 19:52:20 +0200

    continue parsing data

--- data_parsing/parser.py
+++ data_parsing/parser.py
@@ -1,81 +1,129 @@
 import pandas as pd
+import numpy as np
 import os
 
 # Constants
 # ------------------------------------------
-SIMPLE_VECTOR_COLS = ['Overall', 'Potential', 'Crossing', 'Finishing', 'HeadingAccuracy',
-                      'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 'FKAccuracy',
-                      'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 'Agility', 'Reactions', 'Balance',
-                      'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression', 'Interceptions',
-                      'Positioning', 'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 'SlidingTackle']
+SIMPLE_PLAYER_VECTOR = ['Overall', 'Potential', 'Crossing', 'Finishing', 'HeadingAccuracy',
+                        'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 'FKAccuracy',
+                        'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 'Agility', 'Reactions', 'Balance',
+                        'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression', 'Interceptions',
+                        'Positioning', 'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 'SlidingTackle']
 
-COMPLEX_VECTOR_COLS = ['Age', 'Wage', 'International Reputation', 'Work Rate', 'Time In Club', 'Height',
-                       'Weight', 'Release Clause']
+COMPLEX_PLAYER_VECTOR = ['Age', 'Wage', 'International Reputation', 'Work Rate', 'Time In Club', 'Height',
+                         'Weight', 'Release Clause']
+
+UNWANTED_COLS = ['ID', 'Value', 'Wage', 'Special', 'Preferred Foot', 'Weak Foot', 'Skill Moves', 'Body Type']
+
+AVERAGE_COLS = ['Age', 'Value', 'Wage', 'Overall']
+
+# TODO
+SIMPLE_CLUB_VECTOR = []
+COMPLEX_CLUB_VECTOR = []
 
 
 # ------------------------------------------
 
 # Utils methods:
 # ------------------------------------------
+
 def relpath(path):
     return os.path.join(os.path.dirname(__file__), path)
 
 
-# asserts data_frame and string, this will be useful
-# in later process of our coding for debugging
 def assert_df(df, string):
+    # asserts data_frame and string, this will be useful
+    # in later process of coding for debugging purposes
     assert isinstance(string, str) and isinstance(df, pd.core.frame.DataFrame)
 
+def add_average_col(df, col):
+    pass
 
 # ------------------------------------------
 
+# Player methods:
+# ------------------------------------------
 
-# returns rows (== players) of given club
-def get_players_from_club(df, club):
-    assert_df(df, club)
-    return df.loc[df['Club'] == club]
-
-
-def get_club_simple_vector(df, club):
-    pass
 
 def get_player_simple_vector(df, player):
-    assert_df(df, player)
-    return get_player(df, player)[SIMPLE_VECTOR_COLS]
+    # returns simple vector of a single player,
+    # simple is NUMERIC with scaling of [1, 100]
+    return get_player(df, player)[SIMPLE_PLAYER_VECTOR]
 
 
-# returns simple vector of a single player,
-# simple is NUMERIC with scaling of [1, 100]
 def get_player(df, name):
     assert_df(df, name)
     return df.loc[df['Name'] == name]
 
 
-# returns more complex vector of single player,
-# this will include VALUE, hieght, weight transformed to [1, 100] scale:
 def get_player_complex_vector(df, player):
+    # returns more complex vector of single player,
+    #     # this will include VALUE, hieght, weight transformed to [1, 100] scale:
     #  move all to scale [1,100]
     pass
 
 
-# returns all defenders of a team
-# defender can be CB, LD, LR, etc..
 def get_defenders(df, team):
+    # returns all defenders of a team
+    # defender can be CB, LD, LR, etc..
     DEF_POSITIONS = []
     pass
 
 
-# goalkeepers has different data, need to think on this
 def get_goalkeeper_vector(df, gk):
+    # goalkeepers has different data, need to think on this
     pass
 
 
-def pre_process():
-    df = pd.read_csv(relpath('players_f19_edited.csv'))
-    print(get_players_from_club(df, "FC Barcelona").head)
-    print(get_player_simple_vector(df, 'L. Messi'))
+# ------------------------------------------
 
-    # TODO :: read on categorical values
+# Club methods:
+# ------------------------------------------
+def get_club_simple_vector(df, club):
+    pass
 
 
-pre_process()
+def get_players_from_club(df, club):
+    # returns rows (== players) of given club
+    assert_df(df, club)
+    return df.loc[df['Club'] == club]
+
+
+# ------------------------------------------
+
+
+def pre_process(df):
+    # pre-process given DataFrame,
+    # edit col's, add and remove data and SCALE,
+    df.drop(columns=UNWANTED_COLS, inplace=True)
+    df.update(df.select_dtypes(include=[np.number]).fillna(0)) # NaN -> 0 and round to int
+    df.astype(int) #TODO need to move all to int
+    # add_average_col(AVERAGE_COLS)
+
+    # Scaling is chosen as [1,100] and all other numeric values are transformed
+    # to this scaling type:
+
+
+
+def run_example(df):
+    pre_process(df)
+
+
+
+    # df['avgOverall'] = df['Overall'].mean(axis=1)
+
+    # uncomment to run various examples of API's:
+    # --------------------------------------------
+    # barca_players = get_players_from_club(df, 'FC Barcelona')
+    # print(barca_players)
+    player_vec = get_player_simple_vector(df, 'L. Messi')
+    print(player_vec)
+
+
+# ------------------------------------------
+# Usage example:
+# ------------------------------------------
+data = pd.read_csv(relpath('players_f19_edited.csv'))
+run_example(data)
+
+
