68e6ae029e3ea70d9b3f04de3db0f5f3660a251c
omer.jacobi <omer.jacobi@gmail.com>
2019-02-25 21:56:52 +0200

    started cosine simpelarty

--- data_parsing/parser.py
+++ data_parsing/parser.py
@@ -1,25 +1,31 @@
 import pandas as pd
 import numpy as np
 import os
+import re
 
 # Constants
 # ------------------------------------------
-UNWANTED_COLS = ['ID', 'Special', 'Preferred Foot', 'Weak Foot', 'Skill Moves', 'Body Type']
+UNWANTED_COLS = [ 'Special','International Reputation', 'Jersey Number', 'Joined', 'Release Clause']
+
+REMOVE_WAGES = ['Value','Wage']
+
 
 SIGNS_TO_CLEAN = ['€']
-
-SIMPLE_PLAYER_VECTOR = ['Overall', 'Potential', 'Crossing', 'Finishing', 'HeadingAccuracy',
+SIMPLE_PLAYER_VECTOR = ['Name', 'ID','Crossing', 'Finishing', 'HeadingAccuracy',
                         'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 'FKAccuracy',
                         'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 'Agility', 'Reactions', 'Balance',
                         'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression', 'Interceptions',
                         'Positioning', 'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 'SlidingTackle']
+SIMPLE_GK_PLAYER_VECTOR = [ 'Name', 'ID', 'Crossing', 'Finishing', 'HeadingAccuracy',
+                        'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 'FKAccuracy',
+                        'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 'Agility', 'Reactions', 'Balance',
+                        'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression', 'Interceptions',
+                        'Positioning', 'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 'SlidingTackle','GKDiving'
+                             'GKHandling', 'GKKicking', 'GKPositioning' ,'GKReflexes']
 
-COMPLEX_PLAYER_VECTOR = ['Age', 'Wage', 'International Reputation', 'Work Rate', 'Time In Club', 'Height',
-                         'Weight', 'Release Clause']
+SIMPLE_PLAYER_VECTOR_NUMS_RATING = [ 'Weak Foot','Skill Moves','Height','Weight']
 
-SIMPLE_CLUB_VECTOR = ['Age', 'Wage', 'Value', 'Overall', 'Agility']  # generated from players
 
-COMPLEX_CLUB_VECTOR = ['Wins Per Season', 'Loses Per Season', 'Coach Level', 'Team History']  # TODO get his data:
 
 
 # ------------------------------------------
@@ -30,95 +36,96 @@ COMPLEX_CLUB_VECTOR = ['Wins Per Season', 'Loses Per Season', 'Coach Level', 'Te
 def relpath(path):
     return os.path.join(os.path.dirname(__file__), path)
 
+def parse_height(ht_string):
+    """
+    convert height string to float
+    :param ht_string: the string we want to convert
+    :return: height in inches as a number
+    """
+    # format: 7' 0.0"
+    if ht_string is np.nan:
+        return ht_string
+    ht_ = ht_string.split("'")
+    ft_ = float(ht_[0])
+    in_ = float(ht_[1].replace("\"",""))
+    return (12*ft_) + in_
 
+def parse_weight(wt_string):
+    """
+    remove all char in the string and return a float
+    :param wt_string: the string we wnat to remove chars
+    :return: a float number
+    """
+    if wt_string is np.nan:
+        return wt_string
+    num = re.findall(r'\d+',wt_string)
+    return float(num[0])
 # ------------------------------------------
-
 # Player methods:
 # ------------------------------------------
 
 
-def get_player_simple_vector(df, player):
-    # Returns simple vector of a single player, NUMERIC with scaling of [1, 100]
-    # As chosen in constant SIMPLE_VECTOR_PLAYER
-    return get_player(df, player)[SIMPLE_PLAYER_VECTOR].astype(int)
-
-
-def get_player(df, name):
-    return df.loc[df['Name'] == name]
-
-
-def get_player_complex_vector(df, player):
-    # returns more complex vector of single player,
-    # this will include VALUE, height, weight transformed to [1, 100] scale:
-    # TODO
-    pass
-
-
-# ------------------------------------------
-
-# Club methods:
-# ------------------------------------------
-def get_players_from_club(df, club):
-    # returns rows (== players) of given club
-    return df.loc[df['Club'] == club]
-
-
-def get_simple_club_vector(df, club):
-    # Returns average of SIMPLE_CLUB_VECTOR values,
-    # In a new data frame row=[Club] col=[Avg1, avg2,....] form
-    assert set(SIMPLE_CLUB_VECTOR).issubset(df.columns)
-
-    # Init new DF with 0 value and club name:
-    data = np.array(np.zeros((1, len(SIMPLE_CLUB_VECTOR)), dtype=int))
-    cb_df = pd.DataFrame(data, index=[0], columns=SIMPLE_CLUB_VECTOR)
-
-    # Create avg's:
-    players = get_players_from_club(df, club)
-    for col in SIMPLE_CLUB_VECTOR:
-        if col in ['Value', 'Wage'] : continue  # remove this after 'clean signs' is done
-        cb_df[col] = int(players[col].mean())
-
-    # Modify columns and add 'Club'
-    cb_df.columns = "Avg " + cb_df.columns
-    cb_df.insert(loc=0, column='Club', value=club)
-    return cb_df
-
-
-def get_complex_club_vector(df, club):
-    # todo -- simpleVec + complexVec
-    pass
-
-
-# ------------------------------------------
-
-
-def pre_process(df):
-    # pre-process given DataFrame,
-    # Edit col's, add, remove data and SCALE,
+def pre_process(df, goalkeeper=False):
+    """
+    cleaning the pandas data frame by removing duplicates name, unwanted columes
+    :param df: the data fram pandas object
+    :param goalkeeper: if true returns only goalkeepers, otherwise return other player type
+    :return: the data frame after cleaning it up
+    """
     df.drop(columns=UNWANTED_COLS, inplace=True)
-    df.update(df.select_dtypes(include=[np.number]).fillna(0).astype(int))  # make NaN to 0 and make type int #todo not working proply
+    df.drop(columns=REMOVE_WAGES)
+    df["Height"].apply(lambda x:parse_height(x))
+    df["Weight"].apply(lambda x:parse_weight(x))
+    print('totals values: ' + str(len(df['Name'])))
+    if goalkeeper:
+        df = df[df["Position"] == 'GK']
+        df = df[SIMPLE_GK_PLAYER_VECTOR]
+    else:
+        df = df[df["Position"] != 'GK']
+        df = df[SIMPLE_PLAYER_VECTOR]
+    df = df.drop_duplicates(subset=['Name'])
 
-    # Clean Value && Wage € sign
-    # for sign in SIGNS_TO_CLEAN:
-    #     df.apply(lambda row: row.str.strip(sign))
+    print('unique names: ' + str(len(df['Name'])))
+    # df = df[SIMPLE_PLAYER_VECTOR]
+    # df = df.dropna()
+    print('after droping null values: ' + str(len(df['ID'])))
+    return df
+
+
+# ------------------------------------------
+# COSINE DISTANCE:
+# ------------------------------------------
+from sklearn.metrics.pairwise import cosine_similarity
+import math
+def cosine_distance(df: pd.DataFrame):
+    names = df['Name']
+    print(df.head())
+
+
+    # tmpPlayerRating = pd.DataFrame(index=names,columns=names)
+    # print(tmpPlayerRating.head())
+    # for i in range(0, len(tmpPlayerRating.columns)):
+    #     for j in range(0, len(tmpPlayerRating.columns)):
+    tmp_df = df.drop(columns=['Name','ID'])
+    # tmp_df = df.dropna()
+    player_matrix = np.empty([len(names),len(names)])
+    for i, player1 in tmp_df.iterrows():
+        for j, player2 in tmp_df.iterrows():
+            cos = cosine_similarity([player1], [player2])
+            player_matrix[i][j] = cos[0][0]
+            print(i, j,cos[0][0])
+
+
 
 
 def run_example(df):
-    pre_process(df)
-    # uncomment to run various examples of API's:
-    # --------------------------------------------
-    # barca_players = get_players_from_club(df, 'FC Barcelona')
-    # print(barca_players.head)
+    df = pre_process(df)
+    cosine_distance(df)
 
-    # player_vec = get_player_simple_vector(df, 'L. Messi')
-    # print(player_vec)
-
-    print(get_simple_club_vector(df, 'FC Barcelona'))
-    # --------------------------------------------
 
 
 # ------------------------------------------
 # Usage example:
 # ------------------------------------------
-fifa_df = pd.read_csv(relpath('csv\players_f19_edited.csv'))
+fifa_df = pd.read_csv(relpath('csv/players_f19_edited.csv'))
 run_example(fifa_df)
